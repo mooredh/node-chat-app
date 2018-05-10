2ef6e4901983e619579caa201c75a70052566128
Moore <mooredh@gmail.com>
2018-05-11 00:24:52 +0100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+const express = require('express');
+
+let app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+});
